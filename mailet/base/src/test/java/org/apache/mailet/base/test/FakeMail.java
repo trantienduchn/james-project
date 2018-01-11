7cc2622f9659eef0034b6fe78a14d7e7dec246a6
benwa <btellier@linagora.com>
2018-01-11 15:19:34 +0700

    JAMES-2285 FakeMail should handle Per Recipients Header

--- mailet/base/src/test/java/org/apache/mailet/base/test/FakeMail.java
+++ mailet/base/src/test/java/org/apache/mailet/base/test/FakeMail.java
@@ -85,7 +85,8 @@ public class FakeMail implements Mail {
             attributes(mail),
             mail.getMessageSize(),
             mail.getRemoteAddr(),
-            mail.getRemoteHost());
+            mail.getRemoteHost(),
+            mail.getPerRecipientSpecificHeaders());
     }
 
     public static FakeMail from(MimeMessage message) throws MessagingException {
@@ -112,6 +113,7 @@ public class FakeMail implements Mail {
         private Optional<Long> size;
         private Optional<String> remoteAddr;
         private Optional<String> remoteHost;
+        private PerRecipientHeaders perRecipientHeaders;
 
         private Builder() {
             fileName = Optional.empty();
@@ -126,6 +128,7 @@ public class FakeMail implements Mail {
             size = Optional.empty();
             remoteAddr = Optional.empty();
             remoteHost = Optional.empty();
+            perRecipientHeaders = new PerRecipientHeaders();
         }
 
         public Builder size(long size) {
@@ -224,9 +227,14 @@ public class FakeMail implements Mail {
             return this;
         }
 
+        public Builder addHeaderForRecipient(Header header, MailAddress recipient) {
+            this.perRecipientHeaders.addHeaderForRecipient(header, recipient);
+            return this;
+        }
+
         public FakeMail build() throws MessagingException {
             return new FakeMail(getMimeMessage(), recipients, name.orElse(null), sender.orElse(null), state.orElse(null), errorMessage.orElse(null), lastUpdated.orElse(null),
-                attributes, size.orElse(0L), remoteAddr.orElse(DEFAULT_REMOTE_ADDRESS), remoteHost.orElse(DEFAULT_REMOTE_HOST));
+                attributes, size.orElse(0L), remoteAddr.orElse(DEFAULT_REMOTE_ADDRESS), remoteHost.orElse(DEFAULT_REMOTE_HOST), perRecipientHeaders);
         }
 
         private MimeMessage getMimeMessage() throws MessagingException {
@@ -264,7 +272,7 @@ public class FakeMail implements Mail {
     private PerRecipientHeaders perRecipientHeaders;
     
     public FakeMail(MimeMessage msg, List<MailAddress> recipients, String name, MailAddress sender, String state, String errorMessage, Date lastUpdated,
-            Map<String, Serializable> attributes, long size, String remoteAddr, String remoteHost) {
+            Map<String, Serializable> attributes, long size, String remoteAddr, String remoteHost, PerRecipientHeaders perRecipientHeaders) {
         this.msg = msg;
         this.recipients = recipients;
         this.name = name;
@@ -275,7 +283,7 @@ public class FakeMail implements Mail {
         this.attributes = attributes;
         this.size = size;
         this.remoteAddr = remoteAddr;
-        this.perRecipientHeaders = new PerRecipientHeaders();
+        this.perRecipientHeaders = perRecipientHeaders;
         this.remoteHost = remoteHost;
     }
 
