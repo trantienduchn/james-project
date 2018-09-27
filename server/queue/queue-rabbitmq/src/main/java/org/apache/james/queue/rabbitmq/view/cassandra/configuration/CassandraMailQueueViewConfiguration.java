de72e9d053fa0ae6539b579961a1466574e32332
Benoit Tellier <btellier@linagora.com>
2018-09-27 10:42:35 +0700

    JAMES-2544 Add toString to Cassandra MQ View configuration

--- server/queue/queue-rabbitmq/src/main/java/org/apache/james/queue/rabbitmq/view/cassandra/configuration/CassandraMailQueueViewConfiguration.java
+++ server/queue/queue-rabbitmq/src/main/java/org/apache/james/queue/rabbitmq/view/cassandra/configuration/CassandraMailQueueViewConfiguration.java
@@ -22,6 +22,7 @@ package org.apache.james.queue.rabbitmq.view.cassandra.configuration;
 import java.time.Duration;
 import java.util.Objects;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 
 public class CassandraMailQueueViewConfiguration {
@@ -105,5 +106,14 @@ public class CassandraMailQueueViewConfiguration {
     @Override
     public final int hashCode() {
         return Objects.hash(bucketCount, updateBrowseStartPace, sliceWindow);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("bucketCount", bucketCount)
+            .add("updateBrowseStartPace", updateBrowseStartPace)
+            .add("sliceWindow", sliceWindow)
+            .toString();
     }
 }
