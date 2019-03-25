9cf6f4d7c76ad251f2a7423b1ba64151bd1432e5
Tran Tien Duc <dtran@linagora.com>
2019-03-25 15:34:41 +0700

    JAMES-2688 DeletedMessageVault Export API Integration Tests

--- server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/DeletedMessagesVaultTest.java
+++ server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/DeletedMessagesVaultTest.java
@@ -22,6 +22,7 @@ package org.apache.james.jmap.methods.integration;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.with;
 import static org.apache.james.jmap.HttpJmapAuthentication.authenticateJamesUser;
+import static org.apache.james.jmap.JmapCommonRequests.getLastMessageId;
 import static org.apache.james.jmap.JmapCommonRequests.getOutboxId;
 import static org.apache.james.jmap.JmapCommonRequests.listMessageIdsForAccount;
 import static org.apache.james.jmap.JmapURIBuilder.baseUri;
@@ -30,15 +31,24 @@ import static org.apache.james.jmap.TestingConstants.DOMAIN;
 import static org.apache.james.jmap.TestingConstants.LOCALHOST_IP;
 import static org.apache.james.jmap.TestingConstants.calmlyAwait;
 import static org.apache.james.jmap.TestingConstants.jmapRequestSpecBuilder;
+import static org.apache.james.mailbox.backup.ZipAssert.EntryChecks.hasName;
+import static org.apache.james.mailbox.backup.ZipAssert.assertThatZip;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.compress.utils.SeekableInMemoryByteChannel;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.james.GuiceJamesServer;
+import org.apache.james.filesystem.api.FileSystem;
 import org.apache.james.jmap.api.access.AccessToken;
 import org.apache.james.jmap.categories.BasicFeature;
 import org.apache.james.mailbox.DefaultMailboxes;
@@ -47,6 +57,8 @@ import org.apache.james.mailbox.probe.MailboxProbe;
 import org.apache.james.modules.MailboxProbeImpl;
 import org.apache.james.modules.protocols.ImapGuiceProbe;
 import org.apache.james.probe.DataProbe;
+import org.apache.james.server.core.JamesServerResourceLoader;
+import org.apache.james.server.core.filesystem.FileSystemImpl;
 import org.apache.james.utils.DataProbeImpl;
 import org.apache.james.utils.IMAPMessageReader;
 import org.apache.james.utils.JmapGuiceProbe;
@@ -68,6 +80,39 @@ import io.restassured.parsing.Parser;
 import io.restassured.specification.RequestSpecification;
 
 public abstract class DeletedMessagesVaultTest {
+
+    private static class ExportRequest {
+
+        private static class Builder {
+
+            @FunctionalInterface
+            interface RequireSharee {
+                RequireMatchingQuery exportTo(String sharee);
+            }
+
+            @FunctionalInterface
+            interface RequireMatchingQuery {
+                ExportRequest query(String query);
+            }
+        }
+
+        private static Builder.RequireSharee userExportFrom(String userExportFrom) {
+            return sharee -> query -> new ExportRequest(userExportFrom, sharee, query);
+        }
+
+        private final String userExportFrom;
+        private final String sharee;
+        private final String matchingQuery;
+
+        private ExportRequest(String userExportFrom, String sharee, String matchingQuery) {
+            this.userExportFrom = userExportFrom;
+            this.sharee = sharee;
+            this.matchingQuery = matchingQuery;
+        }
+    }
+
+    private static final String FIRST_SUBJECT = "first subject";
+    private static final String SECOND_SUBJECT = "second subject";
     private static final String HOMER = "homer@" + DOMAIN;
     private static final String BART = "bart@" + DOMAIN;
     private static final String PASSWORD = "password";
@@ -79,10 +124,14 @@ public abstract class DeletedMessagesVaultTest {
         "\"combinator\": \"and\"," +
         "\"criteria\": []" +
         "}";
+    private static final ExportRequest EXPORT_ALL_HOMER_MESSAGES_TO_BART = ExportRequest
+        .userExportFrom(HOMER)
+        .exportTo(BART)
+        .query(MATCH_ALL_QUERY);
 
     private MailboxId otherMailboxId;
 
-    protected abstract GuiceJamesServer createJmapServer() throws IOException;
+    protected abstract GuiceJamesServer createJmapServer(FileSystem fileSystem) throws IOException;
 
     protected abstract void awaitSearchUpToDate();
 
@@ -93,10 +142,12 @@ public abstract class DeletedMessagesVaultTest {
     private AccessToken bartAccessToken;
     private GuiceJamesServer jmapServer;
     private RequestSpecification webAdminApi;
+    private FileSystem fileSystem;
 
     @Before
     public void setup() throws Throwable {
-        jmapServer = createJmapServer();
+        fileSystem = new FileSystemImpl(new JamesServerResourceLoader("../DeletedMessagesVaultIntegrationTests/" + UUID.randomUUID()));
+        jmapServer = createJmapServer(fileSystem);
         jmapServer.start();
         MailboxProbe mailboxProbe = jmapServer.getProbe(MailboxProbeImpl.class);
         DataProbe dataProbe = jmapServer.getProbe(DataProbeImpl.class);
@@ -114,12 +165,17 @@ public abstract class DeletedMessagesVaultTest {
         homerAccessToken = authenticateJamesUser(baseUri(jmapServer), HOMER, PASSWORD);
         bartAccessToken = authenticateJamesUser(baseUri(jmapServer), BART, BOB_PASSWORD);
 
-        webAdminApi = WebAdminUtils.spec(jmapServer.getProbe(WebAdminGuiceProbe.class).getWebAdminPort());
+        WebAdminGuiceProbe webAdminGuiceProbe = jmapServer.getProbe(WebAdminGuiceProbe.class);
+        webAdminApi = given()
+            .spec(WebAdminUtils
+                .buildRequestSpecification(webAdminGuiceProbe.getWebAdminPort())
+                .build());
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws Exception {
         jmapServer.stop();
+        FileUtils.forceDelete(fileSystem.getBasedir());
     }
 
     @Category(BasicFeature.class)
@@ -405,6 +461,172 @@ public abstract class DeletedMessagesVaultTest {
             .body(ARGUMENTS + ".list.subject", hasItem(SUBJECT));
     }
 
+    @Category(BasicFeature.class)
+    @Test
+    public void vaultExportShouldSendMailsToShareeWhenJmapDeleteMessage() throws Exception {
+        bartSendMessageToHomer();
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 1);
+        String messageIdOfHomer = listMessageIdsForAccount(homerAccessToken).get(0);
+
+        homerDeletesMessages(listMessageIdsForAccount(homerAccessToken));
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        String fileLocation = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile)
+                .containsOnlyEntriesMatching(hasName(messageIdOfHomer));
+        }
+    }
+
+    @Category(BasicFeature.class)
+    @Test
+    public void vaultExportShouldSendMailsToShareeWhenImapDeleteMessage() throws Exception {
+        bartSendMessageToHomer();
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 1);
+        String messageIdOfHomer = listMessageIdsForAccount(homerAccessToken).get(0);
+
+        imapMessageReader.connect(LOCALHOST_IP, jmapServer.getProbe(ImapGuiceProbe.class).getImapPort())
+            .login(HOMER, PASSWORD)
+            .select(IMAPMessageReader.INBOX)
+            .setFlagsForAllMessagesInMailbox("\\Deleted");
+        imapMessageReader.expunge();
+
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        String fileLocation = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile)
+                .containsOnlyEntriesMatching(hasName(messageIdOfHomer));
+        }
+    }
+
+    @Category(BasicFeature.class)
+    @Test
+    public void vaultExportShouldSendMailToShareeWhenImapDeletedMailbox() throws Exception {
+        bartSendMessageToHomer();
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 1);
+        String messageIdOfHomer = listMessageIdsForAccount(homerAccessToken).get(0);
+
+        imapMessageReader.connect(LOCALHOST_IP, jmapServer.getProbe(ImapGuiceProbe.class).getImapPort())
+            .login(HOMER, PASSWORD)
+            .select(IMAPMessageReader.INBOX);
+
+        imapMessageReader.moveFirstMessage(MAILBOX_NAME);
+
+        imapMessageReader.delete(MAILBOX_NAME);
+
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        String fileLocation = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile)
+                .containsOnlyEntriesMatching(hasName(messageIdOfHomer));
+        }
+    }
+
+    @Test
+    public void vaultExportShouldSendEmailsWithZipContainsOnlyMatchedMessages() throws Exception {
+        bartSendMessageToHomerWithSubject(FIRST_SUBJECT);
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 1);
+        String firstMessageIdOfHomer = listMessageIdsForAccount(homerAccessToken).get(0);
+
+        bartSendMessageToHomerWithSubject(SECOND_SUBJECT);
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 2);
+        String secondMessageIdOfHomer = listMessageIdsForAccount(homerAccessToken).get(1);
+
+        homerDeletesMessages(listMessageIdsForAccount(homerAccessToken));
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        ExportRequest exportRequest = ExportRequest
+            .userExportFrom(HOMER)
+            .exportTo(BART)
+            .query("{" +
+                "  \"fieldName\": \"subject\"," +
+                "  \"operator\": \"equals\"," +
+                "  \"value\": \"" + FIRST_SUBJECT + "\"" +
+                "}");
+        String fileLocation = exportAndGetFileLocationFromLastMail(exportRequest, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile)
+                .containsOnlyEntriesMatching(hasName(firstMessageIdOfHomer));
+        }
+    }
+
+    @Test
+    public void vaultExportShouldSendEmailsWithEmptyZipWhenQueryDoesntMatch() throws Exception {
+        bartSendMessageToHomerWithSubject(FIRST_SUBJECT);
+        bartSendMessageToHomerWithSubject(SECOND_SUBJECT);
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 2);
+
+        homerDeletesMessages(listMessageIdsForAccount(homerAccessToken));
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        ExportRequest exportRequest = ExportRequest
+            .userExportFrom(HOMER)
+            .exportTo(BART)
+            .query("{" +
+                "  \"fieldName\": \"subject\"," +
+                "  \"operator\": \"equals\"," +
+                "  \"value\": \"third subject\"" +
+                "}");
+        String fileLocation = exportAndGetFileLocationFromLastMail(exportRequest, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile).hasNoEntry();
+        }
+    }
+
+    @Test
+    public void vaultExportShouldSendEmailsWithEmptyZipWhenVaultIsEmpty() throws Exception {
+        String fileLocation = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+
+        try (ZipFile zipFile = zipFile(fileSystem.getResource(fileLocation))) {
+            assertThatZip(zipFile).hasNoEntry();
+        }
+    }
+
+    @Test
+    public void vaultExportShouldResponseIdempotentSideEffect() throws Exception {
+        bartSendMessageToHomer();
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 1);
+
+        homerDeletesMessages(listMessageIdsForAccount(homerAccessToken));
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(homerAccessToken).size() == 0);
+
+        String fileLocationFirstExport = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+        String fileLocationSecondExport = exportAndGetFileLocationFromLastMail(EXPORT_ALL_HOMER_MESSAGES_TO_BART, bartAccessToken);
+
+        try (ZipFile firstExportFile = zipFile(fileSystem.getResource(fileLocationFirstExport));
+                ZipFile secondExportFile = zipFile(fileSystem.getResource(fileLocationSecondExport))) {
+            assertThatZip(firstExportFile)
+                .hasSameContentWith(secondExportFile);
+        }
+    }
+
+    private String exportAndGetFileLocationFromLastMail(ExportRequest exportRequest, AccessToken shareeAccessToken) {
+        int currentNumberOfMessages = listMessageIdsForAccount(shareeAccessToken).size();
+        exportVaultContent(exportRequest);
+
+        WAIT_TWO_MINUTES.until(() -> listMessageIdsForAccount(shareeAccessToken).size() == currentNumberOfMessages + 1);
+        String exportingMessageId = getLastMessageId(shareeAccessToken);
+        return exportedFileLocationFromMailHeader(exportingMessageId, shareeAccessToken);
+
+    }
+
+    private String exportedFileLocationFromMailHeader(String messageId, AccessToken accessToken) {
+        return with()
+                .header("Authorization", accessToken.serialize())
+                .body("[[\"getMessages\", {\"ids\": [\"" + messageId + "\"]}, \"#0\"]]")
+                .post("/jmap")
+            .jsonPath()
+                .getList(ARGUMENTS + ".list.headers.corresponding-file", String.class)
+                .get(0);
+    }
+
     private void homerSharesHisMailboxWithBart() {
         with()
             .header("Authorization", homerAccessToken.serialize())
@@ -517,5 +739,23 @@ public abstract class DeletedMessagesVaultTest {
             .body(updateRequestBody)
             .when()
             .post("/jmap");
+    }
+
+    private void exportVaultContent(ExportRequest exportRequest) {
+        String taskId =
+            webAdminApi.with()
+                .body(exportRequest.matchingQuery)
+                .post("/deletedMessages/users/" + exportRequest.userExportFrom + "?action=export&exportTo=" + exportRequest.sharee)
+            .jsonPath()
+                .get("taskId");
+
+        webAdminApi.given()
+                .get("/tasks/" + taskId + "/await")
+            .then()
+                .body("status", is("completed"));
+    }
+
+    private ZipFile zipFile(InputStream data) throws IOException {
+        return new ZipFile(new SeekableInMemoryByteChannel(IOUtils.toByteArray(data)));
     }
 }
