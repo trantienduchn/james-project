6d80aee116e66a29cce4cda499e80cc4822f78e2
Tran Tien Duc <dtran@linagora.com>
2019-03-27 11:39:11 +0700

    fixup! fixup! JAMES-2685 LocalFileBlobExportModule for blob sharing

--- server/protocols/webadmin/webadmin-mailbox-deleted-message-vault/src/main/java/org/apache/james/webadmin/vault/routes/ExportService.java
+++ server/protocols/webadmin/webadmin-mailbox-deleted-message-vault/src/main/java/org/apache/james/webadmin/vault/routes/ExportService.java
@@ -23,16 +23,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.Collection;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.james.blob.api.BlobId;
 import org.apache.james.blob.api.BlobStore;
 import org.apache.james.blob.export.api.BlobExportMechanism;
@@ -43,28 +38,18 @@ import org.apache.james.vault.DeletedMessageVault;
 import org.apache.james.vault.DeletedMessageZipper;
 import org.apache.james.vault.search.Query;
 
-import com.github.fge.lambdas.Throwing;
-import com.github.fge.lambdas.functions.ThrowingFunction;
-import com.github.steveash.guavate.Guavate;
 import com.google.common.annotations.VisibleForTesting;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 class ExportService {
-
-    private class ZippedData {
-        private final long contentLength;
-        private final InputStream inputStream;
-
-        private ZippedData(long contentLength, InputStream content) {
-            this.contentLength = contentLength;
-            this.inputStream = content;
-        }
+    private static File temporaryFile() throws IOException {
+        return Files.createTempFile(TEMPORARY_FILE_NAME_PREFIX, TEMPORARY_FILE_EXTENSION).toFile();
     }
 
     private static final String TEMPORARY_FILE_EXTENSION = ".temp";
+    private static final String TEMPORARY_FILE_NAME_PREFIX = "james-vault-export-";
 
     private final BlobExportMechanism blobExport;
     private final BlobStore blobStore;
@@ -80,53 +65,42 @@ class ExportService {
         this.vault = vault;
     }
 
-    Mono<Void> export(User user, Query exportQuery, MailAddress exportToAddress,
-                      Runnable messageToExportCallback) {
+    void export(User user, Query exportQuery, MailAddress exportToAddress, Runnable messageToExportCallback) throws IOException {
+        Flux<DeletedMessage> matchedMessages = Flux.from(vault.search(user, exportQuery))
+            .doOnNext(any -> messageToExportCallback.run());
+        File file = zipToFile(user, matchedMessages);
 
-        return matchingMessages(user, exportQuery)
-            .doOnNext(any -> messageToExportCallback.run())
-            .collect(Guavate.toImmutableList())
-            .map(Collection::stream)
-            .flatMap(messages -> Mono.fromCallable(() -> zipData(user, messages)))
-            .flatMap(sneakyThrow(zippedData -> blobStore.save(zippedData.inputStream, zippedData.contentLength)))
-            .flatMap(blobId -> exportTo(user, exportToAddress, blobId))
-            .then();
+        try {
+            BlobId blobId = copyIntoBlobStore(file);
+            blobExport.blobId(blobId)
+                .with(exportToAddress)
+                .explanation(exportMessage(user))
+                .export();
+        } finally {
+            FileUtils.forceDelete(file);
+        }
     }
 
-    private Flux<DeletedMessage> matchingMessages(User user, Query exportQuery) {
-        return Flux.from(vault.search(user, exportQuery))
-            .publishOn(Schedulers.elastic());
-    }
-
-    private ZippedData zipData(User user, Stream<DeletedMessage> messages) throws IOException {
+    private File zipToFile(User user, Flux<DeletedMessage> messages) throws IOException {
         File tempFile = temporaryFile();
-        FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
 
-        zipper.zip(message -> loadMessageContent(user, message), messages, fileOutputStream);
-        return new ZippedData(tempFile.length(), new FileInputStream(tempFile));
+        try (FileOutputStream fileOutputStream = new FileOutputStream(tempFile)) {
+            zipper.zip(contentLoader(user), messages.toStream(), fileOutputStream);
+            return tempFile;
+        }
     }
 
-    private File temporaryFile() throws IOException {
-        String tempFileName = UUID.randomUUID().toString();
-        File tempFile = Files.createTempFile(tempFileName, TEMPORARY_FILE_EXTENSION).toFile();
-        tempFile.deleteOnExit();
-        return tempFile;
+    private BlobId copyIntoBlobStore(File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            return blobStore.save(fileInputStream, file.length()).block();
+        }
     }
 
-    private Optional<InputStream> loadMessageContent(User user, DeletedMessage message) {
-        return Mono.from(vault.loadMimeMessage(user, message.getMessageId()))
-            .blockOptional();
+    private DeletedMessageZipper.DeletedMessageContentLoader contentLoader(User user) {
+        return message -> Mono.from(vault.loadMimeMessage(user, message.getMessageId())).blockOptional();
     }
 
-    private Mono<Void> exportTo(User user, MailAddress exportToAddress, BlobId blobId) {
-        return Mono.fromRunnable(() -> blobExport
-            .blobId(blobId)
-            .with(exportToAddress)
-            .explanation(String.format("Some deleted messages from user %s has been shared to you", user.asString()))
-            .export());
-    }
-
-    private <T, R> Function<T, R> sneakyThrow(ThrowingFunction<T, R> function) {
-        return Throwing.function(function).sneakyThrow();
+    private String exportMessage(User user) {
+        return String.format("Some deleted messages from user %s has been shared to you", user.asString());
     }
 }
