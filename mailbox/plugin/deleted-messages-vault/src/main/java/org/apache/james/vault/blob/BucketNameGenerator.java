6ab95840c965c76c5d230db1a98179ee7754cb47
Tran Tien Duc <dtran@linagora.com>
2019-07-10 11:54:07 +0700

    JAMES-2811 BucketNameGenerator should be in charge of determining whether a moment in side a bucket
    Because this component is the place contains a logic to generate bucket names.
    It should be able to extract the date time information from bucket names
    which are made by its self

--- mailbox/plugin/deleted-messages-vault/src/main/java/org/apache/james/vault/blob/BucketNameGenerator.java
+++ mailbox/plugin/deleted-messages-vault/src/main/java/org/apache/james/vault/blob/BucketNameGenerator.java
@@ -20,24 +20,62 @@
 package org.apache.james.vault.blob;
 
 import java.time.Clock;
+import java.time.LocalTime;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
 import org.apache.james.blob.api.BucketName;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class BucketNameGenerator {
+
+    private static final Pattern BUCKET_NAME_PATTERN = Pattern.compile("deleted-messages-([0-9]{4})-([0-9]{2})-(01)");
+    private static final String BUCKET_NAME_GENRATING_FORMAT = "deleted-messages-%d-%02d-01";
+    private static final int FIRST_DAY_OF_MONTH = 1;
+    private static final int ZERO_HOUR = 0;
+    private static final int ZERO_MINUTE = 0;
+    private static final int ZERO_SECOND = 0;
+    private static final int ZERO_NANOSECOND = 0;
+
     private final Clock clock;
 
     @Inject
-    public BucketNameGenerator(Clock clock) {
+    BucketNameGenerator(Clock clock) {
         this.clock = clock;
     }
 
-    public BucketName currentBucket() {
+    BucketName currentBucket() {
         ZonedDateTime now = ZonedDateTime.now(clock);
-        int month = now.getMonthValue();
+        int month = now.getMonth().get(ChronoField.MONTH_OF_YEAR);
         int year = now.getYear();
-        return BucketName.of(String.format("deleted-messages-%d-%02d-01", year, month));
+        return BucketName.of(String.format(BUCKET_NAME_GENRATING_FORMAT, year, month));
+    }
+
+    @VisibleForTesting
+    boolean isBucketRangeBefore(BucketName bucketName, ZonedDateTime beforeThisTime) {
+        return bucketStartingTime(bucketName)
+            .map(bucketStartingTime -> bucketStartingTime.isBefore(firstTimeOfTheMonth(beforeThisTime)))
+            .orElse(false);
+    }
+
+    private ZonedDateTime firstTimeOfTheMonth(ZonedDateTime moment) {
+        return moment.withDayOfMonth(1).with(LocalTime.MIN);
+    }
+
+    private Optional<ZonedDateTime> bucketStartingTime(BucketName bucketName) {
+        return Optional.of(BUCKET_NAME_PATTERN.matcher(bucketName.asString()))
+            .filter(Matcher::matches)
+            .map(matcher -> {
+                int year = Integer.parseInt(matcher.group(1));
+                int month = Integer.parseInt(matcher.group(2));
+                return ZonedDateTime.of(year, month, FIRST_DAY_OF_MONTH, ZERO_HOUR, ZERO_MINUTE,
+                    ZERO_SECOND, ZERO_NANOSECOND, clock.getZone());
+            });
     }
 }
--- mailbox/plugin/deleted-messages-vault/src/test/java/org/apache/james/vault/blob/BucketNameGeneratorTest.java
+++ mailbox/plugin/deleted-messages-vault/src/test/java/org/apache/james/vault/blob/BucketNameGeneratorTest.java
@@ -24,15 +24,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import org.apache.james.blob.api.BucketName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BucketNameGeneratorTest {
     private static final Instant NOW = Instant.parse("2007-12-03T10:15:30.00Z");
-    private static final Instant DATE_2 = Instant.parse("2007-07-03T10:15:30.00Z");
     private static final Clock CLOCK = Clock.fixed(NOW, ZoneId.of("UTC"));
-    private static final Clock CLOCK_2 = Clock.fixed(DATE_2, ZoneId.of("UTC"));
+
+    private static final BucketNameGenerator DEFAULT_GENERATOR = new BucketNameGenerator(CLOCK);
 
     @Test
     void currentBucketShouldReturnBucketFormattedOnFirstDayOfMonth() {
@@ -40,9 +44,89 @@ class BucketNameGeneratorTest {
             .isEqualTo(BucketName.of("deleted-messages-2007-12-01"));
     }
 
-    @Test
-    void monthShouldBeFormattedWithTwoDigits() {
-        assertThat(new BucketNameGenerator(CLOCK_2).currentBucket())
-            .isEqualTo(BucketName.of("deleted-messages-2007-07-01"));
+    @Nested
+    class IsBucketRangeBefore {
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+            "deleted-Messages-2018-07-01",
+            "deleted-Messages-2018-07-02",
+            "deletedMessages-2018-07-01",
+            "deletedMessages-2018-007-01",
+            "deletedMessages-2018-07-001",
+            "2018-07-01",
+            "2018-01-07",
+            "01-07-2018",
+        })
+        void shouldBeFalseWhenPassingNonWellFormattedBucketNames(String bucketNameAsString) {
+            BucketName bucketName = BucketName.of(bucketNameAsString);
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-01-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
+
+        @Test
+        void shouldBeFalseWhenBucketNameIsInSameMonthWithPassedTime() {
+            BucketName bucketName = BucketName.of("deleted-messages-2019-07-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
+
+        @Test
+        void shouldBeFalseWhenBucketNameIsInSameDayWithPassedTime() {
+            BucketName bucketName = BucketName.of("deleted-messages-2019-07-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-01T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
+
+        @Test
+        void shouldBeFalseWhenBucketNameIsInSameTimeWithPassedTime() {
+            BucketName bucketName = BucketName.of("deleted-messages-2019-07-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-01T00:00:00.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
+
+        @Test
+        void shouldBeTrueWhenBucketNameIsInPreviousMonthThanPassedTime() {
+            BucketName bucketName = BucketName.of("deleted-messages-2019-06-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isTrue();
+        }
+
+        @Test
+        void shouldBeTrueWhenBucketNameIsInLongTimeBeforePassedTime() {
+            BucketName bucketName = BucketName.of("deleted-messages-2000-06-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isTrue();
+        }
+
+        @Test
+        void shouldBeFalseWhenBucketNameIsInNearFuture() {
+            BucketName bucketName = BucketName.of("deleted-messages-2019-08-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
+
+        @Test
+        void shouldBeFalseWhenBucketNameIsInFarFuture() {
+            BucketName bucketName = BucketName.of("deleted-messages-2222-08-01");
+            ZonedDateTime beforeThisTime = ZonedDateTime.parse("2019-07-09T10:15:30.00Z");
+
+            assertThat(DEFAULT_GENERATOR.isBucketRangeBefore(bucketName, beforeThisTime))
+                .isFalse();
+        }
     }
 }
